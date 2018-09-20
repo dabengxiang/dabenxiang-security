245429eddd641fd0980b1d613e07f7d395aba504
gyc <836732982@qq.com>
2018-09-20 18:51:53 +0800

    ......

--- dabenxiang-security-app/src/main/java/com/dabenxiang/security/app/ResourceServerConfig.java
+++ dabenxiang-security-app/src/main/java/com/dabenxiang/security/app/ResourceServerConfig.java
@@ -1,8 +1,21 @@
 package com.dabenxiang.security.app;
 
+import com.dabenxiang.security.core.authentication.mobile.SmsCodeConfig;
+import com.dabenxiang.security.core.properties.SecurityConstants;
+import com.dabenxiang.security.core.properties.SecurityProperties;
+import com.dabenxiang.security.core.social.qq.SocialConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfiguration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.social.security.SpringSocialConfigurer;
+
+import javax.jws.Oneway;
 
 /**
  * Date:2018/9/11
@@ -11,8 +24,42 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  */
 @Configuration
 @EnableResourceServer
-public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+    @Autowired
+    private AuthenticationSuccessHandler successHandler;
 
+    @Autowired
+    private AuthenticationFailureHandler failureHandler;
 
+    @Autowired
+    private SecurityProperties securityProperties;
+
+    @Autowired
+    private SmsCodeConfig smsCodeConfig;
+
+    @Autowired
+    private SpringSocialConfigurer springSocialConfigurer;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.formLogin().successHandler(successHandler)
+                .failureHandler(failureHandler)
+                .loginPage(SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_FORM)
+                .and()
+                .apply(smsCodeConfig)
+                .and().apply(springSocialConfigurer)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/authentication/require",
+                        "/code/*",
+                        "/user/regist",
+                        securityProperties.getSocialProperties().getQq().getSigunUpUrl(),
+                        securityProperties.getBrowser().getSession().getSESSION_INVALID_URL(),
+                        securityProperties.getBrowser().getLogOutUrl(),
+                        securityProperties.getBrowser().getLoginPage()).permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().disable();
+
+    }
 }
--- dabenxiang-security-app/src/main/java/com/dabenxiang/security/app/authentication/GycAuthenticationSuccessHandler.java
+++ dabenxiang-security-app/src/main/java/com/dabenxiang/security/app/authentication/GycAuthenticationSuccessHandler.java
@@ -3,10 +3,19 @@ package com.dabenxiang.security.app.authentication;
 import com.dabenxiang.security.core.properties.LoginType;
 import com.dabenxiang.security.core.properties.SecurityProperties;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.collections.MapUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.UnapprovedClientAuthenticationException;
+import org.springframework.security.oauth2.provider.*;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -31,16 +40,65 @@ public class GycAuthenticationSuccessHandler extends SavedRequestAwareAuthentica
     @Autowired
     private ObjectMapper objectMapper;
 
+    private String credentialsCharset = "UTF-8";
+
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    @Autowired
+    private AuthorizationServerTokenServices tokenServices;
+
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
         logger.info("登陆成功");
-        if(LoginType.JSON.equals(securityProperties.getBrowser().getLoginType())){
-            response.setContentType("application/json;chatset=utf-8");
-            response.getWriter().write(objectMapper.writeValueAsString(authentication));
+        String header = request.getHeader("Authorization");
+        if (header != null && header.startsWith("Basic ")) {
+            String[] tokens = this.extractAndDecodeHeader(header, request);
+            assert tokens.length == 2;
+            String clientId = tokens[0];
+            String clientSecret = tokens[1];
+            ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+            if(clientDetails == null){
+                throw new UnapprovedClientAuthenticationException("clientId对应的配置信息不存在:" + clientId);
+            }else if (!clientDetails.getClientSecret().equals(clientSecret)){
+                throw new UnapprovedClientAuthenticationException("clientSecret密码错误" + clientId);
+            }
+
+            TokenRequest tokenRequest = new TokenRequest(MapUtils.EMPTY_MAP,
+                    clientId,clientDetails.getScope(),"custom");
+            OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+            OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(oAuth2Request, authentication);
+            OAuth2AccessToken accessToken = tokenServices.createAccessToken(oAuth2Authentication);
+            response.setContentType("application/json;charset=UTF-8");
+            response.getWriter().write(objectMapper.writeValueAsString(accessToken));
+
         }
-        else
-            super.onAuthenticationSuccess(request,response,authentication);
+
 
     }
 
+    private String[] extractAndDecodeHeader(String header, HttpServletRequest request) throws IOException {
+        byte[] base64Token = header.substring(6).getBytes("UTF-8");
+
+        byte[] decoded;
+        try {
+            decoded = Base64.decode(base64Token);
+        } catch (IllegalArgumentException var7) {
+            throw new BadCredentialsException("Failed to decode basic authentication token");
+        }
+
+        String token = new String(decoded, this.getCredentialsCharset(request));
+        int delim = token.indexOf(":");
+        if (delim == -1) {
+            throw new BadCredentialsException("Invalid basic authentication token");
+        } else {
+            return new String[]{token.substring(0, delim), token.substring(delim + 1)};
+        }
+    }
+
+
+    protected String getCredentialsCharset(HttpServletRequest httpRequest) {
+        return this.credentialsCharset;
+    }
 }
