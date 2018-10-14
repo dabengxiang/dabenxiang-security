92211922d1289d5cc762ed10e4c53b97387af671
aaa <aaa>
2018-10-14 23:02:44 +0800

    ..........

--- sso-demo/sso-server/src/main/java/com/sso/server/SsoAuthorizationServerConfig.java
+++ sso-demo/sso-server/src/main/java/com/sso/server/SsoAuthorizationServerConfig.java
@@ -1,6 +1,5 @@
 package com.sso.server;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -8,11 +7,14 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
+
 
 /**
  * Date:2018/9/25
@@ -21,19 +23,40 @@ import javax.sql.DataSource;
  */
 @Configuration
 @EnableAuthorizationServer
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SsoAuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
 
 
+    @Resource
+    private DataSource dataSource;
+
+
+
+
+
+
+
+
+    @Bean
+    public ClientDetailsService clientDetailsService(){
+        return new JdbcClientDetailsService(dataSource);
+    }
+
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory().withClient("myClient1")
-                .secret("myClient1").scopes("all")
-                .authorizedGrantTypes("authorization_code", "refresh_token")
-                .and().withClient("myClient2")
-                .secret("myClient2").scopes("all")
-                .authorizedGrantTypes("authorization_code", "refresh_token");
+
+
+        clients.withClientDetails(clientDetailsService());
+
+
+//        clients.inMemory().withClient("myClient1")
+//                .secret("myClient1").scopes("all")
+//                .authorizedGrantTypes("authorization_code", "refresh_token")
+//                .and().withClient("myClient2")
+//                .secret("myClient2").scopes("all")
+//                .authorizedGrantTypes("authorization_code", "refresh_token");
 
     }
 
